6eccaf1d4ddbe525afa2c992cfc672c4d1066d3d
jakeliebow1997 <jali1005@colorado.edu>
2019-04-26 13:19:36 -0600

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,65 @@
+import numpy as np
+#data.groupby(['home_team','events']).agg('count')
+#from pybaseball import statcast
+import pandas as pd
+import pymongo
+import boto3
+import pymongo
+#app = Flask(__name__)
+
+import json
+client = pymongo.MongoClient("mongodb+srv://sah:dude@cluster0-ufrcd.mongodb.net/test?retryWrites=true")
+mydb = client["final"]
+mycol = mydb["BA"]
+df=pd.read_csv('data.csv')
+#c = boto3.client(
+#'s3',
+#aws_access_key_id='AKIAJFYPGGLOL4Z3HBHQ',
+#aws_secret_access_key='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'
+#)
+#obj = c.get_object(Bucket='baseballs3', Key='data.csv')
+#data = pd.read_csv(obj['Body'])
+
+def getPlayer(name):
+	mydoc = mycol.find({ "batter": name })
+	for x in mydoc:
+		return x
+
+def main(batter):
+	df=pd.read_csv('data.csv')
+	df=df.loc[df['batter']==batter]#Mookie Betts
+	P=0
+	d={}
+	for i,x in df.iterrows():
+		if type(x['pitch_type'])==type('s'):
+			if x['pitch_type'] not in [*d]:
+				d[x['pitch_type']]=[0,0]
+			if x['description']=='foul' or x['description']=='foul_tip':
+				d[x['pitch_type']][0]+=0.5#foul worth half points as hit
+				d[x['pitch_type']][1]+=1.0
+			if x['description']=='swinging_strike' or x['description']=='swinging_strike_blocked':
+				d[x['pitch_type']][1]+=1.0
+			if x['description']=='hit_into_play' or x['description']=='hit_into_play_no_out' or x['description']=='hit_into_play_score':
+				d[x['pitch_type']][0]+=1.0
+				d[x['pitch_type']][1]+=1.0
+	arr=[]
+	for x in [*d]:
+		arr.append(d[x][0]/d[x][1])
+
+	s=0
+	for x in arr:
+		s+=(x-np.mean(arr))**2
+	d['Batting Adaptability']=s/(len(arr)-1)
+	mydict = { "batter": batter, "data": d }
+	prop=getPlayer(batter)
+	flag=False
+	try:
+		print(len(prop))
+	except:
+		y = mycol.insert_one(mydict)
+
+	return d
+#main(605141.0)#MB
+for x in df.groupby(['batter']).agg('count').index:
+	if x==605141.0:
+		print(main(x))
